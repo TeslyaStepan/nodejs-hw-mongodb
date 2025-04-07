c15746183b380908fa18a26d5e323816a0aa0a6d
Teslya Stepan <stepan199812@gmail.com>
2025-04-07 15:05:09 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { getEnvVar } from "./utils/getEnvVar.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -28,6 +29,7 @@ export const setupServer = async () => {
   app.use(routers);
 
   app.use("/uploads", express.static(UPLOAD_DIR));
+  app.use("/api-docs", swaggerDocs());
 
   app.use("*", notFoundHandler);
 
